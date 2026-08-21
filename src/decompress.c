3f3c225d48a4ad47df3b7d0070584ec74fb3be1a
dpldgr <144766863+dpldgr@users.noreply.github.com>
2026-08-21 12:34:20 +0800

    Export tablebase streams to separate files

--- src/decompress.c
+++ src/decompress.c
@@ -489,20 +489,16 @@ uint8_t *decompress_table(struct tb_handle *H, int bside, int f)
   return table;
 }
 
-struct tb_handle *open_tb_handle(char *tablename, int wdl)
+struct tb_handle *open_tb_file(const char *name, int wdl)
 {
-  size_t len = strlen(tablename) + strlen(wdl ? WDLSUFFIX : DTZSUFFIX) + 1;
-  char *name = malloc(len);
   struct tb_handle *H = malloc(sizeof(struct tb_handle));
 
-  if (!name || !H) {
+  if (!H) {
     fprintf(stderr, "Could not allocate sufficient memory.\n");
     exit(EXIT_FAILURE);
   }
-  snprintf(name, len, "%s%s", tablename, wdl ? WDLSUFFIX : DTZSUFFIX);
   if (!(H->F = fopen(name, "rb"))) {
     fprintf(stderr, "Could not open %s for reading.\n", name);
-    free(name);
     free(H);
     exit(EXIT_FAILURE);
   }
@@ -511,8 +507,22 @@ struct tb_handle *open_tb_handle(char *tablename, int wdl)
   H->data = (uint8_t *)map_file(fd, 1, &(H->mmap));
   close_file(fd);
   H->wdl = wdl;
-  free(name);
 
+  return H;
+}
+
+struct tb_handle *open_tb_handle(char *tablename, int wdl)
+{
+  size_t len = strlen(tablename) + strlen(wdl ? WDLSUFFIX : DTZSUFFIX) + 1;
+  char *name = malloc(len);
+
+  if (!name) {
+    fprintf(stderr, "Could not allocate sufficient memory.\n");
+    exit(EXIT_FAILURE);
+  }
+  snprintf(name, len, "%s%s", tablename, wdl ? WDLSUFFIX : DTZSUFFIX);
+  struct tb_handle *H = open_tb_file(name, wdl);
+  free(name);
   return H;
 }
 
